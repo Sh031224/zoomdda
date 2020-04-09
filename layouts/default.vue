a7f4f28ca80bd33ced247e56d29052ad13b653b0
Sh031224 <1cktmdgh2@gmail.com>
2020-04-09 21:17:20 +0900

    [CREATE] Nuxt-app

---
+++ layouts/default.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const prefersDark =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: Dark)").matches;
+
+    if (prefersDark) {
+      document.getElementsByTagName("html")[0].classList.add("darkmode");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import url("~/assets/style/common.scss");
+
+html {
+  font-family: "Noto Sans Korean", sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="schedule">
+    <footer>
+      <a href="https://github.com/Sh031224" target="blank">
+        © 2020. Sh031224. All rights reserved.
+      </a>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "~/assets/style/color.scss";
+
+.schedule {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: $bg-color;
+  background-color: var(--bg-color);
+}
+footer {
+  text-align: center;
+  a {
+    color: $footer-color;
+    color: var(--footer-color);
+    font-size: 14px;
+    text-decoration: none;
+  }
+}
+</style>

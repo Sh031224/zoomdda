8a55d5c767661f594a5e76020cae029c6198308f
Sh031224 <1cktmdgh2@gmail.com>
2020-04-14 20:32:58 +0900

    [ADD] Localhost https

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,68 @@
+import path from 'path'
+import fs from 'fs'
+
 export default {
-  mode: "spa",
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: process.env.npm_package_name || "",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
-      }
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  router: {
-    middleware: ["auth"]
-  },
-  loading: { color: "#fff" },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: ["~/plugins/vue-sweetalert2.js", "~/plugins/vue-cookie.js"],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    "@nuxtjs/axios"
-  ],
-  /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {},
-  /*
-   ** Build configuration
-   */
-  build: {
+    mode: "spa",
     /*
-     ** You can extend webpack config here
+     ** Headers of the page
      */
-    extend(config, ctx) {}
-  }
+    server: {
+        https: {
+            key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
+            cert: fs.readFileSync(path.resolve(__dirname, 'server.crt'))
+        }
+    },
+    head: {
+        title: process.env.npm_package_name || "",
+        meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            {
+                hid: "description",
+                name: "description",
+                content: process.env.npm_package_description || ""
+            }
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    router: {
+        middleware: ["auth"]
+    },
+    loading: { color: "#fff" },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: ["~/plugins/vue-sweetalert2.js", "~/plugins/vue-cookie.js"],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        "@nuxtjs/axios"
+    ],
+    /*
+     ** Axios module configuration
+     ** See https://axios.nuxtjs.org/options
+     */
+    axios: {},
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
+    }
 };

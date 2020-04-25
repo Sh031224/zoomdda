4452f8ae355d4b32cc7eef877362bd71a13c27bc
Sh031224 <1cktmdgh2@gmail.com>
2020-04-25 17:15:16 +0900

    [FIX] API get

--- pages/school/index.vue
+++ pages/school/index.vue
@@ -13,38 +13,50 @@ export default {
     };
   },
   mounted() {
-    axios
-      .get(`${this.$store.state.url}/auth?school=${this.school}`)
-      .then(res => {
-        axios.defaults.headers.common["school-token"] =
-          res.data.data["school-token"];
-        this.$cookie.set("school_token", res.data.data["school-token"], {
-          expires: 365
+    this.getSchool();
+    setTimeout(() => {
+      this.getSchoolInfo();
+    }, 300);
+  },
+  methods: {
+    async getSchoolInfo() {
+      await axios
+        .get(`${this.$store.state.url}/school`)
+        .then(res => {
+          this.$store.commit(
+            "admin/changeAccess",
+            res.data.data.school.ADMIN_CODE
+          );
+          this.$store.commit("admin/changeAdmin", false);
+          this.$store.commit(
+            "school/addClass",
+            res.data.data.school.CLASS_INFO
+          );
+          this.$store.commit("school/addName", res.data.data.school.NAME);
+          this.$router.push({ path: "/select" });
+        })
+        .catch(error => {
+          console.log(error);
+
+          alert("알 수 없는 오류가 발생했습니다.");
+          this.$router.push({ path: "/close" });
         });
-        axios
-          .get(`${this.$store.state.url}/school`)
-          .then(res => {
-            this.$store.commit(
-              "admin/changeAccess",
-              res.data.data.school.ADMIN_CODE
-            );
-            this.$store.common("admin/changeAdmin", false);
-            this.$store.commit(
-              "school/addClass",
-              res.data.data.school.CLASS_INFO
-            );
-            this.$store.commit("school/addName", res.data.data.school.NAME);
-            this.$router.push({ path: "/select" });
-          })
-          .catch(() => {
-            alert("알 수 없는 오류가 발생했습니다.");
-            this.$router.push({ path: "/close" });
+    },
+    async getSchool() {
+      axios
+        .get(`${this.$store.state.url}/auth?school=${this.school}`)
+        .then(res => {
+          axios.defaults.headers.common["school-token"] =
+            res.data.data["school-token"];
+          this.$cookie.set("school_token", res.data.data["school-token"], {
+            expires: 365
           });
-      })
-      .catch(() => {
-        alert("학교에서 알려준 URL로 접속해주세요.");
-        this.$router.push({ path: "/close" });
-      });
+        })
+        .catch(() => {
+          alert("학교에서 알려준 URL로 접속해주세요.");
+          this.$router.push({ path: "/close" });
+        });
+    }
   }
 };
 </script>
